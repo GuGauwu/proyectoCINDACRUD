6f83bcc0a5f3d387cf5420f4e6d0eab97a062da1
GuGauwu <gustavo.mayoral85@unach.mx>
2025-03-28 12:16:54 -0600

    apiobtener

--- obtener_alertas.php
+++ obtener_alertas.php
@@ -1,31 +1,25 @@
 <?php
-// llamara conexión
-require_once("conexión_oracle.php");
+// link de la API
+$url = "https://sysweb.unach.mx/Siae/api/Alertas/Obtener";
 
-// Preparar la llamada
-$stid = oci_parse($conn, "BEGIN PKG_ALERTAS.Obt_Grid_Alertas(:p_registros); END;");
+// Hacemos la solicitud GET a la API
+$response = file_get_contents($url);
 
-
-// Definir el cursor como parámetro de salida
-$cursor = oci_new_cursor($conn);
-oci_bind_by_name($stid, ":p_registros", $cursor, -1, OCI_B_CURSOR);
-
-// Ejecutar
-oci_execute($stid);
-oci_execute($cursor);
-
-// Leer
-$alertas = [];
-while ($fila = oci_fetch_assoc($cursor)) {
-    $alertas[] = $fila; 
+//validacion de la respuesta
+if ($response === FALSE) {
+    echo json_encode(["error" => "No se pudo obtener los datos de la API"]);
+    exit;
 }
-// Cerrar conexiones
-oci_free_statement($stid);
-oci_free_statement($cursor);
-oci_close($conn);
 
-// Mostrar los resultados en pantalla (para pruebas)
-echo "<pre>";
-print_r($alertas);
-echo "</pre>";
+// Convertir a JSON en un array PHP
+$datos = json_decode($response, true);
+
+// verificamos si los datos se decodificaron correctamente
+if ($datos === null) {
+    echo json_encode(["error" => "Error al decodificar JSON"]);
+    exit;
+}
+// Enviar los datos a la tabla en formato JSON
+header("Content-Type: application/json");
+echo json_encode($datos);
 ?>
